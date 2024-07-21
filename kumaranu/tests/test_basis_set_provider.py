30e448abe5f92feceb4b4550c1ad55c684cbcf40
Anup Kumar <anukumar94663@gmail.com>
2024-07-20 19:39:54 -0700

    Testing multiple basis sets.

--- kumaranu/tests/test_basis_set_provider.py
+++ kumaranu/tests/test_basis_set_provider.py
@@ -11,7 +11,8 @@ project_root = Path(__file__).resolve().parents[2]
 
 @pytest.fixture()
 def setup_test_environment(tmp_path, request):
-    input_geom = read(project_root / "kumaranu" / "tests" / '16.xyz')
+    # input_geom = read(project_root / "kumaranu" / "tests" / '16.xyz')
+    input_geom = read(project_root / "kumaranu" / "tests" / 'molecule_xyz_files' / '04_C1H2O1_geometry_1.xyz')
 
     def cleanup():
         files_and_dirs = [
@@ -37,10 +38,23 @@ def setup_test_environment(tmp_path, request):
 def test_nwchem_ase_calc(setup_test_environment):
     input_ase_obj = setup_test_environment
     # Ignoring my error in the scratch directory assignment for now and cleaning it up afterwards
-    input_ase_obj.calc = NWChem(dft=dict(maxiter=2000,
-                                xc='B3LYP'),
-                                basis='6-31+G*')
-    assert input_ase_obj.get_potential_energy() == pytest.approx(-7394.730594653764, abs=1e-5)
+    input_ase_obj.calc = NWChem(dft=dict(maxiter=2000, xc='B3LYP'), basis='STO-3G')
+    err_percent = (input_ase_obj.get_potential_energy()/27.2114 - (-114.38086655385808))/(-114.38086655385808)*100
+    print('')
+    print(err_percent)
+    input_ase_obj.calc = NWChem(dft=dict(maxiter=2000, xc='B3LYP'), basis='3-21G')
+    err_percent = (input_ase_obj.get_potential_energy()/27.2114 - (-114.38086655385808))/(-114.38086655385808)*100
+    print(err_percent)
+    input_ase_obj.calc = NWChem(dft=dict(maxiter=2000, xc='B3LYP'), basis='6-31G')
+    err_percent = (input_ase_obj.get_potential_energy()/27.2114 - (-114.38086655385808))/(-114.38086655385808)*100
+    print(err_percent)
+    input_ase_obj.calc = NWChem(dft=dict(maxiter=2000, xc='B3LYP'), basis='6-31G*')
+    err_percent = (input_ase_obj.get_potential_energy()/27.2114 - (-114.38086655385808))/(-114.38086655385808)*100
+    print(err_percent)
+    # assert input_ase_obj.get_potential_energy() == pytest.approx(-7394.730594653764, abs=1e-5)
+    #assert input_ase_obj.get_potential_energy() == pytest.approx(-3115.4232282956423, abs=1e-5)
+    err_percent = (input_ase_obj.get_potential_energy()/27.2114 - (-114.38086655385808))/(-114.38086655385808)*100
+    assert err_percent == pytest.approx(0.01, abs=1e-2)
 
 
 def test_get_basis_set_high_precision(setup_test_environment):
